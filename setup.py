dbd368445f9b5e98853f4c407ddac55c4ec8a0c4
Daniel Kotschate <daniel@d3v0.de>
2018-08-31 13:24:32 +0200

    Minimal structure for python packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pybamcmap',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/dkotscha/pybamcmap',
+      author='Daniel Kotschate',
+      author_email='daniel@d3v0.de',
+      license='MIT',
+      packages=['pybamcmap'],
+      zip_safe=False)
