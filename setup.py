dd9681fdb302cede31cb9b050dbbdd15a1cf40f2
Daniel Kotschate <daniel@d3v0.de>
2018-09-03 19:58:06 +0200

    Added more colormaps and project description.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pybamcmap',
-      version='0.1',
-      description='The funniest joke in the world',
+      version='0.2',
+      description='A collection of colormaps using the CI of the Bundesanstalt für Materialforschung und -prüfung (BAM).',
       url='https://github.com/dkotscha/pybamcmap',
       author='Daniel Kotschate',
       author_email='daniel@d3v0.de',
