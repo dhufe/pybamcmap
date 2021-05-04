86bd38feaf3dbc8e7fbd553e0692d2e4792f58e1
Daniel Hufschläger <daniel@hufschlaeger.net>
2021-05-04 06:12:37 +0200

    Updating pypi config by adding the README as project description.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pybamcmap',
-      version='0.3',
-      description='A collection of colormaps using the CI of the Bundesanstalt für Materialforschung und -prüfung (BAM).',
+      version='0.3.1',
+      description='A collection of colormaps using the CD of the Bundesanstalt für Materialforschung und -prüfung (BAM).',
       url='https://github.com/dhufe/pybamcmap',
       author='Daniel Hufschläger',
       author_email='daniel@hufschlaeger.net',
       license='MIT',
       packages=['pybamcmap'],
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
       zip_safe=False)
